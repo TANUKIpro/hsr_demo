a469a2c731b4435594220936691c5038263a116a
TANUKIpro <ryotaro_koike@outlook.com>
2017-11-10 12:06:02 +0900

    up data

--- scripts/hsr_demo.py
+++ scripts/hsr_demo.py
@@ -112,6 +112,74 @@ class Demo():
             tts.say('faild what can i do')
             tts.language = tts.JAPANESE
             omni_base.go_abs(0, 0, 0, 180.0)
+            
+class Grep_the_bottle():
+    def __init__(self):
+        tts.language = tts.ENGLISH
+        tts.say('go to table')
+        omni_base.go(0.18838, 1.60785, -3.08466,20,relative=False)
+        
+        self.bridge = CvBridge()
+        self.pinhole= image_geometry.PinholeCameraModel()
+        self._tf_buffer = tf2_ros.Buffer()
+        self._listener = tf2_ros.TransformListener(self._tf_buffer)
+        self.pinhole_info = rospy.Subscriber('/hsrb/head_rgbd_sensor/rgb/camera_info' , CameraInfo , self.camera, queue_size = 1)
+        
+        objsub = message_filters.Subscriber('/objects', ObjectArray)
+        depthsub = message_filters.Subscriber('/hsrb/head_rgbd_sensor/depth_registered/image', Image)
+        image_person = message_filters.Subscriber('/hsrb/head_rgbd_sensor/rgb/image_color', Image) 
+        ts = message_filters.ApproximateTimeSynchronizer([objsub, depthsub , image_person], 100 , 0.3)
+        ts.registerCallback(self.find_person)
+        
+    def camera(self, data):
+        self.pinhole.fromCameraInfo(data)
+        
+    def find_object(self, object, depth, image):
+        result_array = []
+        for pobj in object.objects:
+           if (self.flag):
+               break
+           person_probability = tr.probability(pobj.class_probability, "smelling bottle")
+           person_probability *= pobj.objectness
+           if person_probability < 0.001:
+               continue
+           
+           top = pobj.top
+           left = pobj.left
+           bottom = pobj.bottom
+           right = pobj.right
+           change_dep = self.bridge.imgmsg_to_cv2(depth, desired_encoding="passthrough")
+           self.depth = change_dep
+           self.person_follow(top , bottom , right , left , self.depth , object)
+        
+    def distance_from_bottle(self , top , bottom , right , left , depth , objarr):
+        y = int((top + bottom)/2)
+        x = int((left + right)/2)
+        ray = self.pinhole.projectPixelTo3dRay([x , y])
+        camera_point = np.array(ray) * depth[y , x]
+
+        pointstamped = PointStamped()
+
+        pointstamped.header.stamp = rospy.Time.now()
+        pointstamped.header.frame_id = objarr.header.frame_id
+        pointstamped.point.x = camera_point[0]
+        pointstamped.point.y = camera_point[1]
+        pointstamped.point.z = camera_point[2]
+
+        try:
+            pointstamped = self._tf_buffer.transform(pointstamped , "base_footprint" , timeout = rospy.Duration(5))
+            print pointstamped.point
+            tts.say("try to take the bottle")
+            whole_body.move_end_effector_pose(((pointstamped.point.x+0.02,pointstamped.point.y,pointstamped.point.z),(math.sqrt(2)/2,0,math.sqrt(2)/2,0)))
+            gripper.grasp(-0.01)
+            omni_base.go(-0.4,0,0,10,relative=True)
+            whole_body.move_to_go()
+            self.flag = True
+            omni_base.go(1.48420, 1.75561, 1.56843,20,relative=False)
+            whole_body.move_to_neutral()
+            gripper.command(1)
+        except:
+            rospy.logerr('faild')
         
 def main():
     demo = Demo()
